fe6cbf16b2cb947be8ef7c4ad770b5e6708b2d56
Vitaliy Semenov <synthex@bk.ru>
2023-06-08 15:28:23 +0600

    feat: implement getting single category

--- laravel-simple-api/app/Http/Controllers/Api/CategoryController.php
+++ laravel-simple-api/app/Http/Controllers/Api/CategoryController.php
@@ -3,13 +3,20 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\CategoryResource;
 use App\Models\Category;
-use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class CategoryController extends Controller
 {
-    public function index(): Collection
+    public function index(): AnonymousResourceCollection
     {
-        return Category::all();
+        $categories = Category::all();
+        return CategoryResource::collection($categories);
+    }
+
+    public function show(Category $category): CategoryResource
+    {
+        return new CategoryResource($category);
     }
 }
